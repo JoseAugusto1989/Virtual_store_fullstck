94de16dd42ad2deb95095a200d03198569257654
Jose Augusto de Oliveira Pins <joliveirapins.jaop@gmail.com>
2023-04-01 11:23:48 -0300

    first commit

---
+++ backend/src/main/java/com/store/backend/controllers/Hello.java
@@ -0,0 +1,17 @@
+package com.store.backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello springboot " + new Date();
+    }
+}
